160737e1b11d48b4d17d4fdc570c041335e99954
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-03-31 17:57:02 -0500

    Nuevas condicionales
    Nuevas condicionales para guardar y modificar las respuestas

--- vista/js/usuario/gestionusuario.js
+++ vista/js/usuario/gestionusuario.js
@@ -5,7 +5,7 @@ var usuarioModelo = {
 };
 var gestionUsuario = {
     constructor: function () {
-        $('#frm').on('submit', gestionUsuario.guardarRespuesta);
+        gestionUsuario.consultatrespuestas();
         $('.modelo').click(function () {
             var data = {};
             data.tabla = $(this).attr("data-tabla");
@@ -16,33 +16,59 @@ var gestionUsuario = {
     repuestaConsultarPreg: function (respuesta) {
         var cuerpo = $('#contenido_preguntas');
         var titulo = $("#exampleModalLabel1");
-        var modal = $('#prgModal');
         cuerpo.empty();
         titulo.empty();
-        (respuesta.datos != "") ? modal.modal('show') : "";
+
+        var modal = $('#prgModal');
+        (respuesta.datos != "") ? modal.modal('show'): "";
 
         var datos = respuesta.datos;
         usuarioModelo.listaUsuarios = datos;
         for (var i = 0; i < datos.length; i++) {
             var registro = datos[i];
             var preguntas = registro.preguntas;
-            titulo.text(registro.tabla + " - " + registro.contenido);
-            var reg = preguntas.split("|");
+            var respuestas = registro.respuestas;
+            var button = $('#accion_btn');
 
+            titulo.text(registro.tabla + " - " + registro.contenido);
+
+            if (registro.id_usuario == registro.usuario) {
+                var btn = '<button class="btn btn-aqua text-dark modificar" href="#!">Guardar Puntaje</button>';
+            } else {
+                var btn = '<button class="btn btn-aqua text-dark grabar" href="#!">Guardar Puntaje</button>';
+            }
+            button.html(btn);
+
+            var reg = preguntas.split("|");
             for (var p = 0; p < reg.length; p++) {
                 var pos = reg[p];
                 var fila = '<div class="row">' +
                     '<div class="col-lg-11 col-sm-10 col-9">' +
                     '<span>' + pos + '</span>' +
                     '</div>' +
-                    '<div class="col-lg-1 col-sm-2 col-3">' +
-                    '<input type="number" class="form-control" name="pos" min="1" max="5" required autocomplete="off"/>' +
-                    '</div>' +
+                    '<div class="col-lg-1 col-sm-2 col-3">';
+
+                if (registro.id_usuario == registro.usuario) {
+                    fila += '<input type="number" class="form-control unoalcinco" min="1" name="pos" value="' + registro.respta[p] + '" required autocomplete="off"/>';
+
+                } else {
+                    fila += '<input type="number" class="form-control unoalcinco" min="1" name="pos" required autocomplete="off"/>';
+                }
+
+                fila += '</div>' +
                     '</div>' +
                     '<hr class="bg-light ">';
                 cuerpo.append(fila);
             }
         }
+        $('.unoalcinco').on('input', function () {
+            var value = $(this).val();
+            if ((value !== '') && (value.indexOf('.') === -1)) {
+                $(this).val(Math.max(Math.min(value, 5), -5));
+            }
+        });
+        $('.grabar').on('click', gestionUsuario.guardarRespuesta);
+        $('.modificar').on('click', gestionUsuario.modificarRespuesta);
     },
     guardarRespuesta: function (e) {
         e.preventDefault();
@@ -56,14 +82,43 @@ var gestionUsuario = {
             'respuestas': respuestas,
             'opcion': 'guardar_respuesta'
         };
-        app.ajax('../controlador/GestionUsuarioControlador.php?opcion=guardar_respuesta', data, gestionUsuario.respuestaGuardarPregunta);
+        app.ajax('../controlador/GestionUsuarioControlador.php?opcion=guardar_respuesta', data, gestionUsuario.respuestaPregunta);
     },
-    respuestaGuardarPregunta: function (respuesta) {
-        swal({
-            title: "perfecto",
-            text: respuesta.mensaje,
-            type: "success"
-        });
+    modificarRespuesta: function (e) {
+        e.preventDefault();
+        var formulario = $('#frm');
+        var dat = formulario.serialize();
+        var res = dat.split("&pos=").join("|");
+        var respuestas = res.replace("pos=", "");
+
+        var data = {
+            'id_respuestas': usuarioModelo.listaUsuarios[0].id_respuestas,
+            'respuestas': respuestas,
+            'opcion': 'modificar_respuesta'
+        };
+        app.ajax('../controlador/GestionUsuarioControlador.php?opcion=modificar_respuesta', data, gestionUsuario.respuestaPregunta);
+    },
+    respuestaPregunta: function (respuesta) {
+        if (respuesta.codigo = 1) {
+            $('#prgModal').modal('hide');
+            swal({
+                title: "perfecto",
+                text: respuesta.mensaje,
+                type: "success"
+            });
+        } else {
+            swal({
+                title: "Error",
+                type: "error"
+            });
+        }
+    },
+    consultatrespuestas: function (e) {
+        var data = {};
+        app.ajax('../controlador/GestionUsuarioControlador.php?opcion=consultar_datos', data, gestionUsuario.respuestaConsultatpreguntas);
+    },
+    respuestaConsultatpreguntas: function (respuesta) {
+        console.log(respuesta);
     }
 };
 gestionUsuario.constructor();
