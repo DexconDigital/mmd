13651881613b0906c56042b78a51ffe6e528fe95
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-04-08 09:24:25 -0500

    Nuevas consultas para traer de la BD

--- modelo/dao/UsuarioDao.php
+++ modelo/dao/UsuarioDao.php
@@ -32,12 +32,23 @@ class UsuarioDAO {
     }
 
     public function consultar_preguntas( $tabla, $contenido ) {
-        $sql = "select prg.*, res.respuestas, res.id_usuario, res.id_respuestas from preguntas prg LEFT JOIN respuestas res on prg.id_pregunta = res.id_pregunta 
-                WHERE prg.tabla=:tabla AND prg.contenido=:contenido";
+        $sql = "select * from preguntas WHERE tabla=:tabla AND contenido=:contenido";
         $sentencia = $this->cnn->prepare( $sql );
         $sentencia->execute( [
             'tabla' => $tabla,
             'contenido' => $contenido
+        ] );
+        return $sentencia->fetchAll( PDO::FETCH_OBJ );
+    }
+
+    public function consultar_respuestas( $tabla, $contenido, $id_usuario ) {
+        $sql = "select prg.*, res.respuestas, res.id_usuario, res.id_respuestas from preguntas prg LEFT JOIN respuestas res on prg.id_pregunta = res.id_pregunta 
+                WHERE prg.tabla=:tabla AND prg.contenido=:contenido AND res.id_usuario=:id_usuario";
+        $sentencia = $this->cnn->prepare( $sql );
+        $sentencia->execute( [
+            'tabla' => $tabla,
+            'contenido' => $contenido,
+            'id_usuario' => $id_usuario
         ] );
         return $sentencia->fetchAll( PDO::FETCH_OBJ );
     }
@@ -53,23 +64,32 @@ class UsuarioDAO {
         ] );
         return $this->cnn->lastInsertId();
     }
-    
-    public function modificar_respuesta($id_respuestas, $respuestas) {
+
+    public function modificar_respuesta( $id_respuestas, $respuestas ) {
         $sql = "UPDATE respuestas SET respuestas=:respuestas WHERE id_respuestas=:id_respuestas";
-        $sentencia = $this->cnn->prepare($sql);
-        $sentencia->execute([
+        $sentencia = $this->cnn->prepare( $sql );
+        $sentencia->execute( [
             'id_respuestas' => $id_respuestas,
             'respuestas' => $respuestas
-        ]);
+        ] );
         return $this->cnn->lastInsertId();
     }
-    
+
     public function consultar_datos( $id_usuario ) {
-        $sql = "SELECT res.id_pregunta, prg.tabla, res.respuestas FROM preguntas prg INNER JOIN respuestas res on res.id_pregunta = prg.id_pregunta WHERE res.id_usuario =:id_usuario ORDER BY (prg.tabla)";
+        $sql = "SELECT res.id_pregunta, prg.tabla, prg.preguntas, prg.calculos_1, prg.contenido, prg.estandar, res.respuestas FROM preguntas prg INNER JOIN respuestas res on res.id_pregunta = prg.id_pregunta WHERE res.id_usuario =:id_usuario ORDER BY (prg.tabla)";
         $sentencia = $this->cnn->prepare( $sql );
         $sentencia->execute( [
             'id_usuario' => $id_usuario
         ] );
         return $sentencia->fetchAll( PDO::FETCH_OBJ );
+    }
+
+    public function consultar_usuario( $id_usuario ) {
+        $sql = "SELECT razon_social, nit FROM usuario WHERE id_usuario =:id_usuario";
+        $sentencia = $this->cnn->prepare( $sql );
+        $sentencia->execute( [
+            'id_usuario' => $id_usuario
+        ] );
+        return $sentencia->fetch( PDO::FETCH_OBJ );
     }
 }
